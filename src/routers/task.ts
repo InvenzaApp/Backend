9f4e9e11095ddf2dbd8e932eb71f8246c3db6e40
konradkrawczyk <konrad.krawczyk@mytracko.pl>
2025-04-17 17:29:49 +0200

    BAC-12 Upload files

--- src/routers/task.ts
+++ src/routers/task.ts
@@ -1,6 +1,7 @@
 import {Router} from "express";
 import {INVALID_REQUEST_PARAMETERS} from "../helpers/response-codes";
 import {TaskModule} from "../modules/task-module";
+import {performFailureResponse, performSuccessResponse} from "../helpers/responses";
 
 const router = Router();
 const taskModule = new TaskModule();
@@ -9,13 +10,13 @@ router.post('/', (req, res) => {
     const { title, description, deadline, groupsIdList } = req.body;
 
     if(!title || !groupsIdList){
-        res.status(401).send({'success': false, 'data': INVALID_REQUEST_PARAMETERS});
+        performFailureResponse(res, INVALID_REQUEST_PARAMETERS);
         return;
     }
 
     const taskId = taskModule.addTask(title, description, deadline, groupsIdList);
 
-    res.status(200).send({'success': true, 'data': taskId});
+    performSuccessResponse(res, taskId);
 });
 
 router.put('/:id', (req, res) => {
@@ -23,18 +24,18 @@ router.put('/:id', (req, res) => {
     const { title, description, deadline, groupsIdList } = req.body;
 
     if(!title || !groupsIdList){
-        res.status(401).send({'success': false, 'data': INVALID_REQUEST_PARAMETERS});
+        performFailureResponse(res, INVALID_REQUEST_PARAMETERS);
         return;
     }
 
     const taskId = taskModule.updateTask(id, title, description, deadline, groupsIdList);
 
-    res.status(200).send({'success': true, 'data': taskId});
+    performSuccessResponse(res, taskId);
 });
 
 router.get('/', (req, res) => {
     const tasks = taskModule.getTasks();
-    res.status(200).send({'success': true, 'data': tasks});
+    performSuccessResponse(res, tasks);
 });
 
 router.get('/:id', (req, res) => {
@@ -42,7 +43,7 @@ router.get('/:id', (req, res) => {
 
     const task = taskModule.getTask(resourceId);
 
-    res.status(200).send({'success': true, 'data': task});
+    performSuccessResponse(res, task);
 });
 
 router.delete('/:id', (req, res) => {
@@ -50,7 +51,7 @@ router.delete('/:id', (req, res) => {
 
     taskModule.deleteTask(resourceId);
 
-    res.status(200).send({'success': true, 'data': null});
+    performSuccessResponse(res, resourceId);
 });
 
 export default router;
