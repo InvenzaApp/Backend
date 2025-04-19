30bb308af9bf7e63162fc93ff35116b245f58313
konradkrawczyk <konrad.krawczyk@mytracko.pl>
2025-04-19 14:41:20 +0200

    IN-51 Delay for debugging

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -3,10 +3,13 @@ import UserModule from "../modules/user-module";
 import {INVALID_CREDENTIALS, INVALID_REQUEST_PARAMETERS} from "../helpers/response-codes";
 import {performFailureResponse, performSuccessResponse} from "../helpers/responses";
 import {TokenManager} from "../managers/token-manager";
+import {delay} from "../helpers/delay";
+require("dotenv").config();
 
 const router = Router();
 const tokenManager = new TokenManager();
 const userModule = new UserModule();
+const isDebug = process.env.DEBUG;
 
 router.post('/sign-in', async (req, res) => {
     const { email, password } = req.body;
@@ -19,11 +22,19 @@ router.post('/sign-in', async (req, res) => {
     const userData = userModule.signIn(email, password);
 
     if(!userData) {
+        if(isDebug){
+            await delay(2000);
+        }
+
         performFailureResponse(res, INVALID_CREDENTIALS);
         return;
     }
 
     const token = tokenManager.getAccessToken(userData.id);
+
+    if(isDebug){
+        await delay(2000);
+    }
     performSuccessResponse(res, userData.toJson(), token);
 });
 
