3979ae7d85738ab2810b0edc45b1665b46c96f53
Konrad <konrad.krawczyk@icloud.com>
2025-04-24 07:47:46 +0000

    IN-79 Extended fakers

--- src/fakers/task.ts
+++ src/fakers/task.ts
@@ -1,12 +1,63 @@
-import {userFaker} from "./user";
 import {DateTime} from "../helpers/date-time";
+import { randomFutureDate } from "./dates";
+import { adminFaker, moderatorFaker } from "./user";
 
-export const taskFaker = {
+export const taskFaker1 = {
     'id': 0,
-    'title': "Przykładowe zadanie",
-    'description': "Przykładowy opis podstawowego zadania",
-    'deadline': null,
+    'title': "Utworzyć zespół",
+    'description': "Dokończyć tworzenie zespołu",
+    'deadline': DateTime.getDateTimestamp(randomFutureDate()),
     'groupsIdList': [0],
     'createdAt': DateTime.getFullTimestamp(),
-    'createdById': userFaker.id,
+    'createdById': adminFaker.id,
+};
+
+export const taskFaker2 = {
+    'id': 1,
+    'title': "Nadzór",
+    'description': "Skończyć nadzór zespołu",
+    'deadline': DateTime.getDateTimestamp(randomFutureDate()),
+    'groupsIdList': [1],
+    'createdAt': DateTime.getFullTimestamp(),
+    'createdById': moderatorFaker.id,
+};
+
+export const taskFaker3 = {
+    'id': 2,
+    'title': "Tabela danych",
+    'description': "Wpisać wszystkie zarobki do tabelki",
+    'deadline': DateTime.getDateTimestamp(randomFutureDate()),
+    'groupsIdList': [0, 1, 2, 3],
+    'createdAt': DateTime.getFullTimestamp(),
+    'createdById': adminFaker.id,
+};
+
+export const taskFaker4 = {
+    'id': 3,
+    'title': "Weryfikacja dokumentów",
+    'description': "Sprawdzić poprawność wszystkich dokumentów",
+    'deadline': DateTime.getDateTimestamp(randomFutureDate()),
+    'groupsIdList': [0, 2, 3],
+    'createdAt': DateTime.getFullTimestamp(),
+    'createdById': moderatorFaker.id,
+};
+
+export const taskFaker5 = {
+    'id': 4,
+    'title': "Testowanie aplikacji",
+    'description': "Przeprowadzić pełne testy wersji beta",
+    'deadline': DateTime.getDateTimestamp(randomFutureDate()),
+    'groupsIdList': [0, 1, 2, 3],
+    'createdAt': DateTime.getFullTimestamp(),
+    'createdById': adminFaker.id,
+};
+
+export const taskFaker6 = {
+    'id': 5,
+    'title': "Zebranie zespołu",
+    'description': "Zorganizować spotkanie zespołu projektowego",
+    'deadline': DateTime.getDateTimestamp(randomFutureDate()),
+    'groupsIdList': [0, 1, 2, 3],
+    'createdAt': DateTime.getFullTimestamp(),
+    'createdById': moderatorFaker.id,
 };
