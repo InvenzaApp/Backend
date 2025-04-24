aacd431d51d8ad10e464281cebcfef610cc40a09
Konrad <konrad.krawczyk@icloud.com>
2025-04-24 11:02:47 +0000

    IN-94 Turn on debug compatibility

--- src/fakers/organization.ts
+++ src/fakers/organization.ts
@@ -1,5 +1,12 @@
 import {adminFaker, moderatorFaker, pmFaker, taskPreviewFaker, workerFaker} from "./user";
 
+export const defaultOrganization = {
+    'id': 0,
+    'title': 'Invenza',
+    'usersIdList': [adminFaker.id],
+    'adminId': adminFaker.id,
+}
+
 export const organizationFaker = {
     'id': 0,
     'title': "Invenza",
